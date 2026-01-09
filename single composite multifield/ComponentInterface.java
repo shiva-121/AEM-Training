444d68f951bcca3d23f0859e81a236226a897ebf
Shivam Sharma <163172373+shiva-121@users.noreply.github.com>
2026-01-09 18:01:44 +0530

    Update ComponentInterface.java

--- single composite multifield/ComponentInterface.java
+++ single composite multifield/ComponentInterface.java
@@ -1,11 +1,7 @@
-package com.adobe.ase.aem.training.core.models;
-
-import java.util.List;
-
 import com.adobe.cq.wcm.core.components.models.Component;
 
 public interface ComponentInterface extends Component{
 	public List<CompositeMultifieldInterface> getCompositeMultified();
+
+	// Add methods for other fields with suitable datatype and provide mapping implementation in impl class
 }
-
-
