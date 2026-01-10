70cf71178912bbf0b642b7f4eff2ae62e88d61af
Shivam Sharma <163172373+shiva-121@users.noreply.github.com>
2026-01-10 15:04:14 +0530

    Update ComponentInterfaceIMPL.java

--- single composite multifield/ComponentInterfaceIMPL.java
+++ single composite multifield/ComponentInterfaceIMPL.java
@@ -1,6 +1,5 @@
 @Model(adaptables = {SlingHttpServletRequest.class, Resource.class},
 adapters = {ComponentInterface.class, ComponentExporter.class},
-defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
 resourceType = "/apps/ase/components/path-to-component")
 	
 public class ComponentInterfaceIMPL implements ComponentInterface{
@@ -14,4 +13,5 @@ public class ComponentInterfaceIMPL implements ComponentInterface{
 	}
 
 }
+
 
